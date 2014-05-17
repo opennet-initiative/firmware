71b0ed1a6d513727909812fc5e740c73afe3f966
Lars Kruse <devel@sumpfralle.de>
2014-05-17 18:08:51 +0200

    Opennet-Init-Skript verbessert
    * dhcp-fwd wird nicht mehr im Hintergrund gestartet (warum sollte es?)
    * nameserver-Dateitest vereinfacht ("-s" genuegt)
    * Quoting von Variablen korrigiert
    * Skript explizit mit exitcode Null beenden

--- opennet/packages/on-openvpn/files/usr/sbin/opennet_up.sh
+++ opennet/packages/on-openvpn/files/usr/sbin/opennet_up.sh
@@ -17,30 +17,31 @@ if [ -e /tmp/openvpn_msg.txt ]; then
 fi
 echo "vpn-tunnel active" >/tmp/openvpn_msg.txt	# a short message for the web frontend
 
-. $IPKG_INSTROOT/usr/bin/on-helper.sh
+. "$IPKG_INSTROOT/usr/bin/on-helper.sh"
 
 if [ -z "$(ip rule show | grep "lookup tun")" ];then
-    mainprio=$(ip rule show | awk 'BEGIN{FS="[: ]"} /main/ {print $1; exit}')
+	mainprio=$(ip rule show | awk 'BEGIN{FS="[: ]"} /main/ {print $1; exit}')
 	for network in $(echo "$(uci get -q firewall.zone_local.network) $(uci get -q firewall.zone_free.network)"); do
-        networkprefix=$(get_network $network)
-        [ -n "$networkprefix" ] && ip rule add from $networkprefix table tun prio $((mainprio+10))
-    done
-	ip rule add iif lo table tun prio $((mainprio+10))
+		networkprefix=$(get_network $network)
+		[ -n "$networkprefix" ] && ip rule add from "$networkprefix" table tun prio "$((mainprio+10))"
+	done
+	ip rule add iif lo table tun prio "$((mainprio+10))"
 fi
 
 ip route flush table tun
-#   prefer olsrd-routes for main and tunnel network
+# prefer olsrd-routes for main and tunnel network
 for network in $(uci get on-core.defaults.on_network); do
-    ip route prepend throw $network table tun
+	ip route prepend throw "$network" table tun
 done
-ip route add default via $route_vpn_gateway table tun
+ip route add default via "$route_vpn_gateway" table tun
 
-if [ -f "/etc/init.d/dhcp-fwd" ]; then
-	. /etc/init.d/dhcp-fwd
-	start &
-fi
+# start dhcp-fwd early
+# TODO: why? It should run automatically anyway ...
+[ -f "/etc/init.d/dhcp-fwd" ] && /etc/init.d/dhcp-fwd start
 
-# At the beginning of system start /etc/resolv.conf.auto is not available or size=0. In that case start service
-if [ ! -e "/tmp/resolv.conf.auto" -o ! -s "/tmp/resolv.conf.auto" ]; then
-        update_dns_servers                
-fi
+# Empty or non-existing nameserver file? Discover opennet nameservers immediately ...
+[ ! -s "/tmp/resolv.conf.auto" ] && update_dns_servers
+
+# always finish with success
+exit 0
+

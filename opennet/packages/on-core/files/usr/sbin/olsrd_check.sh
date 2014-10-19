a6c2422248d94fb002831f260c8c17f05b515663
Lars Kruse <devel@sumpfralle.de>
2014-10-19 04:50:23 +0200

    Umstieg auf die neue banner-Funktion

--- opennet/packages/on-core/files/usr/sbin/olsrd_check.sh
+++ opennet/packages/on-core/files/usr/sbin/olsrd_check.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
+. "${IPKG_INSTROOT:-}/usr/bin/on-helper.sh"
+
+# Ist OLSR zwischenzeitlich abgestuerzt?
 if [ -z "$(pidof olsrd)" ]; then
-    /etc/init.d/olsrd start
-    if [ -n "$(pidof olsrd)" ]; then
-        echo " - $(date) - olsrd restart  -----" >>/etc/banner; sync
-    fi
+    /etc/init.d/olsrd start >/dev/null
+    sleep 1
+    [ -n "$(pidof olsrd)" ] && add_banner_event "olsrd restart"
 fi
+

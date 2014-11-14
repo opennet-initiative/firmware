33e91623a9ceac8ad82bc57a24f55dd96066ef6d
Lars Kruse <devel@sumpfralle.de>
2014-11-15 00:47:38 +0100

    Build/Release-Erkennung nachgebessert

--- tools/trac-bitten-slave/export-build.sh
+++ tools/trac-bitten-slave/export-build.sh
@@ -39,11 +39,11 @@ esac
 step=0
 while read line; do
   case "$line" in
-    "PKG_VERSION:="*) 
+    "ONI_PKG_VERSION:="*) 
       VERSION="${line#*=}" 
       step=$((step +1))
       ;;
-    "PKG_RELEASE:="*) 
+    "ONI_PKG_RELEASE:="*) 
       RELEASE="${line#*=}"
       step=$((step +1))
       ;;

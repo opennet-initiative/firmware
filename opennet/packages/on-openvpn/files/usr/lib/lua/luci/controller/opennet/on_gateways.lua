1d739408a013e45109c5edc5192e41f7a9128c5b
Lars Kruse <devel@sumpfralle.de>
2014-12-18 13:41:44 +0100

    on_vpn_autosearch entfernt

--- opennet/packages/on-openvpn/files/usr/lib/lua/luci/controller/opennet/on_gateways.lua
+++ opennet/packages/on-openvpn/files/usr/lib/lua/luci/controller/opennet/on_gateways.lua
@@ -13,7 +13,6 @@ You may obtain a copy of the License at
 $Id: opennet.lua 5485 2009-11-01 14:24:04Z jow $
 ]]--
 module("luci.controller.opennet.on_gateways", package.seeall)
-require ("luci.model.opennet.on_vpn_autosearch")
 require("luci.model.opennet.funcs")
 
 function index()

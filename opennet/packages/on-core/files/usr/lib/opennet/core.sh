f75aac5b548636c2cab11549d50adef070f4096a
Lars Kruse <devel@sumpfralle.de>
2014-12-27 04:55:11 +0100

    "ucarp"-Fehlermeldungen ignorieren

--- opennet/packages/on-core/files/usr/lib/opennet/core.sh
+++ opennet/packages/on-core/files/usr/lib/opennet/core.sh
@@ -546,6 +546,10 @@ get_potential_error_messages() {
 	#    Falls noch kein Interface vorhanden ist (z.B. als wifi-Client), dann taucht diese Meldung
 	#    beim Booten auf.
 	filters="${filters}|olsrd.*startup-error"
+	# 7) ucarp
+	#    Beim Booten tauchen Fehlermeldungen aufgrund nicht konfigurierter Netzwerk-Interfaces auf.
+	#    TODO: ucarp nur noch als nachinstallierbares Paket markieren (erfordert Aenderung der Makefile-Erzeugung)
+	filters="${filters}|ucarp"
 	# System-Fehlermeldungen (inkl. "trapped")
 	logread | grep -i error | grep -vE "(${filters#|})" || true
 }

ac6ea517d7a51e79f19e769742c21f7d9d09b2a5
Lars Kruse <devel@sumpfralle.de>
2015-03-03 03:33:52 +0100

    nach Mesh-VPN-Aufbau das olsr-Interface konfigurieren

--- opennet/packages/on-usergw/files/usr/lib/opennet/events/mesh_openvpn_updown.sh
+++ opennet/packages/on-usergw/files/usr/lib/opennet/events/mesh_openvpn_updown.sh
@@ -2,8 +2,7 @@
 #
 # Opennet Firmware
 #
-# Copyright 2010 Rene Ejury <opennet@absorb.it>
-# Copyright 2014 Lars Kruse <devel@sumpfralle.de>
+# Copyright 2015 Lars Kruse <devel@sumpfralle.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +12,46 @@
 #
 
 
+set -eu
+
 . "${IPKG_INSTROOT:-}/usr/lib/opennet/on-helper.sh"
 
+
+setup_mesh_interface() {
+	local ifname="$1"
+	local netname="$ifname"
+	uci set "network.${netname}=interface"
+	uci set "network.${netname}.proto=none"
+	uci set "network.${netname}.ifname=$ifname"
+	add_interface_to_zone "$ZONE_MESH" "$netname"
+	update_olsr_interfaces
+	apply_changes network
+	apply_changes firewall
+}
+
+
+cleanup_mesh_interface() {
+	local ifname="$1"
+	local netname="$ifname"
+	uci_delete "network.${netname}"
+	del_interface_from_zone "$ZONE_MESH" "$netname"
+	update_olsr_interfaces
+	apply_changes network
+	apply_changes firewall
+}
+
+
 log_openvpn_events_and_disconnect_if_requested "mesh-openvpn-connections"
+
+
+case "$script_type" in
+	up)
+		setup_mesh_interface "$dev"
+		;;
+	down)
+		cleanup_mesh_interface "$dev"
+		;;
+esac
+
+exit 0
 

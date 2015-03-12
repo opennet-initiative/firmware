a004e5c2a0c6f48cc738c68d351957e26c786600
Lars Kruse <devel@sumpfralle.de>
2015-03-12 05:02:43 +0100

    nur die Dateien der installierten Pakete übertragen

--- opennet/tools/copy-package-files-to-AP.sh
+++ opennet/tools/copy-package-files-to-AP.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 #
 # Kopiere die Entwicklungsdateien aus dem lokalen Baum auf einen Ziel-AP
+# Es werden nur Dateien für diejenigen Pakete übertragen, die auf dem Ziel installiert sind.
 # Der einzige erforderliche Paramter ist der Ziel-Host.
 
 set -eu
 
-TARGET_HOST="$1"
+TARGET_HOST="root@$1"
 BASE_DIR=$(cd "$(dirname "$0")/.."; pwd)
-SRC_PACKAGES="on-core on-openvpn on-usergw"
 
 RSYNC_OPTS="-ax --exclude '.*.swp'"
 SCP_OPTS="-rp"
 
+
+# wir wollen nur Dateien fuer diejenigen Pakete übertragen, die tatsächlich installiert sind
+get_installed_opennet_packages() {
+	ssh "$TARGET_HOST" "opkg list-installed" | grep ^on- | awk '{ print $1 }'
+}
+
+
 # Konstruiere die Parameterliste
 get_source_and_target_params_null_terminated() {
 	# Quellpfade
-	for pkg in $SRC_PACKAGES; do
+	get_installed_opennet_packages | while read pkg; do
 		# Verwende null-terminated strings fuer xargs
 		printf "%s\0" "$BASE_DIR/packages/$pkg/files/."
 	done
 	# der Zielpfad
-	printf "%s\0" "root@${TARGET_HOST}:/"
+	printf "%s\0" "${TARGET_HOST}:/"
 }
+
 
 get_source_and_target_params_null_terminated | xargs -0 -- rsync $RSYNC_OPTS && exit 0 || echo "rsync failed - falling back to slow scp"
 get_source_and_target_params_null_terminated | xargs -0 -- scp $SCP_OPTS

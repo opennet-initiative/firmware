e640c09da99f6e550ab7e7fdf9141a951aa2e2dd
Lars Kruse <devel@sumpfralle.de>
2015-06-11 05:42:51 +0200

    korrekte UGW-Statusausgabe bei fehlender Verbindung

--- opennet/packages/on-usergw/files/usr/lib/lua/luci/controller/opennet/on_usergw.lua
+++ opennet/packages/on-usergw/files/usr/lib/lua/luci/controller/opennet/on_usergw.lua
@@ -108,7 +108,7 @@ function status_ugw_connection()
         end
         ugw_status.mesh_servers_string = string_join(ugw_status.mesh_servers, ", ")
         ugw_status.mesh_servers_status = (ugw_status.mesh_servers_string ~= "")
-	if ugw_status.mesh_servers_string ~= "" then
+	if ugw_status.mesh_servers_string and ugw_status.mesh_servers_string ~= "" then
             result = luci.i18n.translate("Connected Mesh Gateways: ") .. ugw_status.mesh_servers_string
         else
             result = luci.i18n.translate("No Mesh Gateways connected")

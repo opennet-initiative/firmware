037661bbb27e89f98243a1decb2cac668817af00
Lars Kruse <devel@sumpfralle.de>
2015-09-02 02:14:14 +0200

    Ausführung von "clear_caches" nach jeder Kopie

--- opennet/tools/copy-package-files-to-AP.sh
+++ opennet/tools/copy-package-files-to-AP.sh
@@ -31,7 +31,9 @@ get_source_and_target_params_null_terminated() {
 }
 
 
-get_source_and_target_params_null_terminated | xargs -0 -- rsync $RSYNC_OPTS && exit 0 || echo "rsync failed - falling back to slow scp"
-get_source_and_target_params_null_terminated | xargs -0 -- scp $SCP_OPTS
+get_source_and_target_params_null_terminated | xargs -0 -- rsync $RSYNC_OPTS || {
+	echo "rsync failed - falling back to slow scp"
+	get_source_and_target_params_null_terminated | xargs -0 -- scp $SCP_OPTS
+}
 # vorsichtshalber: luci-Neustart und shell-Modul-Cleanup
 ssh "$TARGET_HOST" "on-function clear_caches"

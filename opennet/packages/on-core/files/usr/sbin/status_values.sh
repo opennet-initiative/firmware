21dedeede86f763b90c66058aaab2a17b18fbc92
Lars Kruse <devel@sumpfralle.de>
2015-09-12 02:11:19 +0200

    ondataservice-Sammler auf neue Funktionen umgestellt
    außerdem ein paar Korrekturen bezüglich IPv6-Adressen

--- opennet/packages/on-core/files/usr/sbin/status_values.sh
+++ opennet/packages/on-core/files/usr/sbin/status_values.sh
@@ -16,11 +16,22 @@
 # Fehler sind akzeptabel
 set +e
 
+# In diesem Skript ist ausnahmsweise die Konstruktionen "local foo=$(bar)" erlaubt.
+# Typischerweise maskiert diese Konstruktion den Exit-Code von "bar" und ist daher zu vermeiden.
+# Siehe http://mywiki.wooledge.org/BashPitfalls#local_varname.3D.24.28command.29
+# In diesem Skript tolerieren wir jedoch Fehler, da nicht klar geprueft wird, welche Module installiert wurden.
+
 
 DATABASE_VERSION="0.2"          # we start with 0.2 to have space for the old firmware database, which is 0.1
 DATABASE_FILE="/tmp/database"   # ".json" will be added if it is exported as JSON
 EXPORT_JSON="1"                 # export as JSON-file, does not require sqlite3/libsqlite
                                 # if you change this, ensure that sqlite3 is installed
+
+# Zeilenumbrueche durch Leerzeichen entfernen; fuehrende und abschliessende Leerzeichen anschliessend entfernen
+join() {
+	tr '\n' ' ' | sed 's/^ *//; s/ *$//'
+}
+
 
 create_database() {
   [ -f "$DATABASE_FILE" ] && return
@@ -62,15 +73,16 @@ create_database() {
 
 print_interfaces_2_6() {
   olsr_interfaces=$(on-function request_olsrd_txtinfo int)
-  for if_name in $(ls /sys/class/net/ | awk '!/lo/ {print $0}'); do
+  dhcp_fwd=
+  for if_name in $(ls /sys/class/net/ | grep -v "^lo$"); do
     iface_up=$(cat "/sys/class/net/${if_name}/operstate")
     # sometimes the up-state is not recognized by /sys, than check with 'ip link'
     if [ "$iface_up" = "unknown" ] && [ -n "$(ip link show "$if_name" | awk '{if ($2 == "'$if_name':" && $3 ~ "UP") print $0}')" ]; then
         iface_up="up"
     fi
-    if_type_bridge="$(ls "/sys/class/net/${if_name}/brif" 2>/dev/null | awk '{printf $1" "}')"
-    ip_addr=$(ip address show "$if_name" | awk 'BEGIN{ORS=" "} /inet/ {print $2}')  #   contains additional whitespace
-    if_type_bridgedif=$([ -e "/sys/class/net/${if_name}/brport/bridge" ] && echo "1" || echo "0")    # is bridged interface
+    if_type_bridge=$(ls "/sys/class/net/${if_name}/brif" 2>/dev/null | awk '{print $1}' | join)
+    ip_addr=$(ip address show "$if_name" | awk '/inet6? / {print $2}' | join)
+    if_type_bridgedif=$([ -e "/sys/class/net/${if_name}/brport/bridge" ] && echo "1" || echo "0")
 
     if [ "$iface_up" = "up" ] && ([ -n "$ip_addr" ] || [ -n "$if_type_bridgedif" ]) || [ -n "$if_type_bridge" ]; then
       if [ -e "/sys/class/net/${if_name}/wireless/" ]; then
@@ -119,22 +131,24 @@ print_interfaces_2_6() {
 
       if_hwaddr=$(cat "/sys/class/net/${if_name}/address" 2>/dev/null)
 
-      ip_broadcast=$(ip address show "$if_name" | awk 'BEGIN{ORS=" "} /inet/ {print $4}')  #   contains additional whitespace
-      ip_label=$(ip address show "$if_name" | awk 'BEGIN{ORS=" "} /inet/ {print $NF}')  #   contains additional whitespace
+      ip_broadcast=$(ip address show "$if_name" | awk '/inet / {print $4}' | join)
+      ip_label=$(ip address show "$if_name" | awk '/inet / {print $NF}' | join)
       # opennet-firmware / openwrt values
+      on_networks=
+      on_zones=
+      lastnetwork=
       all_networks=$(uci -q show network | awk 'BEGIN{FS="="} /network\..*\.ifname/ {if ($2 == "'${if_name##br-}'") {gsub("network.",""); gsub(".ifname",""); print$1;}}')
-      on_networks=""; on_zones=""; lastnetwork="";
       for network in $all_networks; do
-        if [ -n "$network" ] && [ "$(uci_get network.$network.proto)" != "none" ]; then
-          on_networks="$on_networks $network"
+        if [ -n "$network" ] && [ "$(uci_get "network.$network.proto")" != "none" ]; then
+          on_networks="${on_networks:+$on_networks }$network"
           lastnetwork="$network"
           zone=$(get_zone_of_interface "$network")
-          on_zones="$on_zones $zone"
+          on_zones="${on_zones:+$on_zones }$zone"
         fi
       done
       if [ -n "$if_type_bridge" ]; then
         zone=$(get_zone_of_interface "${if_name##br-}")
-        on_zones="$on_zones $zone"
+        on_zones="${on_zones:+$on_zones }$zone"
       fi
       dhcpignore="$(uci_get "dhcp.${lastnetwork}.ignore")"
       dhcp_start="$([ "$dhcpignore" = "1" ] || uci_get dhcp.${lastnetwork}.start)"
@@ -142,19 +156,17 @@ print_interfaces_2_6() {
       dhcp_leasetime="$([ "$dhcpignore" = "1" ] || uci_get dhcp.${lastnetwork}.leasetime)"
 
       if [ -e /etc/dhcp-fwd.conf ]; then
-          dhcp_fwd="${dhcpfwd:-} $(awk 'BEGIN{out=0} {if ($1 == "if" && $2 == "'$if_name'" && $3 == "true") out=1;
+          dhcp_fwd="${dhcp_fwd:+$dhcp_fwd }$(awk 'BEGIN{out=0} {if ($1 == "if" && $2 == "'$if_name'" && $3 == "true") out=1;
                   if (out == 1 && $1 == "server" && $2 == "ip") printf $3}' /etc/dhcp-fwd.conf)"
-      else
-          dhcp_fwd=""
       fi
       on_olsr="$(echo "$olsr_interfaces" | awk 'BEGIN{out="0"} {if ($1 == "'$if_name'") {out="1"; end};} END{print out;}')"
 
       SQL_STRING="$SQL_STRING
         INSERT INTO ifaces VALUES
-        ('', '$on_olsr_mainip', '$if_name', '${if_type_bridge% }', '$if_type_bridgedif', '$if_hwaddr',
-        '${ip_label% }', '${ip_addr% }', '${ip_broadcast% }',
-        '${on_networks# }', '${on_zones# }', '$on_olsr',
-        '$dhcp_start', '$dhcp_limit', '$dhcp_leasetime', '${dhcp_fwd# }',
+        ('', '$on_olsr_mainip', '$if_name', '$if_type_bridge', '$if_type_bridgedif', '$if_hwaddr',
+        '$ip_label', '$ip_addr', '$ip_broadcast',
+        '$on_networks', '$on_zones', '$on_olsr',
+        '$dhcp_start', '$dhcp_limit', '$dhcp_leasetime', '$dhcp_fwd',
         '$ifstat_collisions', '$ifstat_rx_compressed', '$ifstat_rx_errors',
         '$ifstat_rx_length_errors', '$ifstat_rx_packets', '$ifstat_tx_carrier_errors',
         '$ifstat_tx_errors', '$ifstat_tx_packets', '$ifstat_multicast',
@@ -180,13 +192,13 @@ print_interfaces_2_6() {
 "wlan_txpower":"%s","wlan_signal":"%s","wlan_noise":"%s","wlan_bitrate":"%s","wlan_crypt":"%s","wlan_vaps":"%s","db_ver":"%s","db_update":"%s"}}\n
 EOF
 )
-        printf $JSON "" "$on_olsr_mainip" "$if_name" "${if_type_bridge% }" "$if_type_bridgedif" "$if_hwaddr" "${ip_label% }" "${ip_addr% }" "${ip_broadcast% }" \
-		"${on_networks# }" "${on_zones# }" "$on_olsr" "$dhcp_start" "$dhcp_limit" "$dhcp_leasetime" "${dhcp_fwd# }" "$ifstat_collisions" "$ifstat_rx_compressed" "$ifstat_rx_errors" \
+        printf $JSON "" "$on_olsr_mainip" "$if_name" "$if_type_bridge" "$if_type_bridgedif" "$if_hwaddr" "$ip_label" "$ip_addr" "$ip_broadcast" \
+		"$on_networks" "$on_zones" "$on_olsr" "$dhcp_start" "$dhcp_limit" "$dhcp_leasetime" "$dhcp_fwd" "$ifstat_collisions" "$ifstat_rx_compressed" "$ifstat_rx_errors" \
 		"$ifstat_rx_length_errors" "$ifstat_rx_packets" "$ifstat_tx_carrier_errors" "$ifstat_tx_errors" "$ifstat_tx_packets" "$ifstat_multicast" "$ifstat_rx_crc_errors" \
 		"$ifstat_rx_fifo_errors" "$ifstat_rx_missed_errors" "$ifstat_tx_aborted_errors" "$ifstat_tx_compressed" "$ifstat_tx_fifo_errors" "$ifstat_tx_window_errors" \
 		"$ifstat_rx_bytes" "$ifstat_rx_dropped" "$ifstat_rx_frame_errors" "$ifstat_rx_over_errors" "$ifstat_tx_bytes" "$ifstat_tx_dropped" "$ifstat_tx_heartbeat_errors" \
 		"$wlan_essid" "$wlan_apmac" "$wlan_type" "$wlan_hwmode" "$wlan_mode" "$wlan_channel" "$wlan_freq" "$wlan_txpower" "$wlan_signal" "$wlan_noise" "$wlan_bitrate" \
-		"$wlan_crypt" "$wlan_vaps" "$DATABASE_VERSION" "$db_epoch" | tr '\n' ' ' >>${DATABASE_FILE}.json.tmp
+		"$wlan_crypt" "$wlan_vaps" "$DATABASE_VERSION" "$db_epoch" | join >>${DATABASE_FILE}.json.tmp
 	    echo >>${DATABASE_FILE}.json.tmp	
       fi
     fi
@@ -205,29 +217,29 @@ SQL_STRING="$SQL_STRING
 
 print_interfaces_2_6
 
-sys_ver="$(uname -sr)"
-sys_board="$(cat /proc/diag/model 2>/dev/null || awk 'BEGIN{FS="[ \t]+:[ \t]";ORS=" "} /machine|Model|system type|Hardware/ {print $2;}' /proc/cpuinfo)"
-sys_cpu="$(awk 'BEGIN{FS="[ \t]+:[ \t]";ORS=" "} /Processor|cpu model|vendor_id/ {print $2;}' /proc/cpuinfo)"
-sys_mem="$(awk '{if ($1 == "MemTotal:") {print $2}}' /proc/meminfo)"
-sys_uptime="$(cat /proc/uptime | awk '{print $1}')"
-sys_load="$(awk '{print $1" "$2" "$3}' /proc/loadavg)"
-sys_free="$(awk '{if ($1 == "MemFree:") {print $2}}' /proc/meminfo)"
-sys_watchdog="$(pidof watchdog >/dev/null && echo "1" || echo "0")"
+sys_ver=$(uname -sr)
+sys_board=$(cat /proc/diag/model 2>/dev/null || awk 'BEGIN{FS="[ \t]+:[ \t]"} /machine|Model|system type|Hardware/ {print $2;}' /proc/cpuinfo | join)
+sys_cpu=$(awk 'BEGIN{FS="[ \t]+:[ \t]"} /Processor|cpu model|vendor_id/ {print $2;}' /proc/cpuinfo | join)
+sys_mem=$(awk '{if ($1 == "MemTotal:") {print $2}}' /proc/meminfo)
+sys_uptime=$(cat /proc/uptime | awk '{print $1}')
+sys_load=$(awk '{print $1" "$2" "$3}' /proc/loadavg)
+sys_free=$(awk '{if ($1 == "MemFree:") {print $2}}' /proc/meminfo)
+sys_watchdog=$(pidof watchdog >/dev/null && echo "1" || echo "0")
 
 . /etc/openwrt_release
 sys_os_type="$DISTRIB_ID"
 sys_os_name="$DISTRIB_CODENAME"
 sys_os_rel="$DISTRIB_RELEASE"
-sys_os_ver="$(opkg status base-files | awk '{if (/Version/) printf $2;}')"
-sys_os_arc="$(opkg status base-files | awk '{if (/Architecture/) printf $2;}')"
-sys_os_insttime="$(opkg status base-files | awk '{if (/Installed-Time/) printf $2;}')"
+sys_os_ver=$(opkg status base-files | awk '{if (/Version/) printf $2;}')
+sys_os_arc=$(opkg status base-files | awk '{if (/Architecture/) printf $2;}')
+sys_os_insttime=$(opkg status base-files | awk '{if (/Installed-Time/) printf $2;}')
 
-on_core_ver="$(opkg status on-core | awk '{if (/Version/) printf $2;}')"
-on_core_insttime="$(opkg status on-core | awk '{if (/Installed-Time/) printf $2;}')"
-on_packages="$(opkg status | awk '{if ($1 == "Package:" && $2 ~ "^on-" && $2 != "on-core") printf $2" "}')"
+on_core_ver=$(opkg status on-core | awk '{if (/Version/) printf $2;}')
+on_core_insttime=$(opkg status on-core | awk '{if (/Installed-Time/) printf $2;}')
+on_packages=$(opkg status | awk '{if ($1 == "Package:" && $2 ~ "^on-" && $2 != "on-core") print $2}' | join)
 
 on_olsrd_status="$(pidof olsrd >/dev/null && echo "1" || echo "0")"
-on_olsr_mainip="$(on-function request_olsrd_txtinfo config | awk '/MainIp/ {print $2}')"
+on_olsr_mainip="$(on-function request_olsrd_txtinfo "config" | awk '/MainIp/ {print $2}')"
 
 
 if is_function_available "captive_portal_get_property"; then
@@ -251,12 +263,12 @@ if is_function_available "get_active_mig_connections"; then
 			age=$(get_mig_connection_test_age "$service_name")
 			status=$(get_service_value "$service_name" "status")
 			echo "${gw_ipaddr}:${status}:${age}"
-		done | tr '\n' ' ')
+		done | join)
 
 	# liste alle deaktivierten Dienste auf
 	on_vpn_blist=$(get_services "gw" | while read service_name; do
 			uci_is_true "$(get_service_value "$service_name" "disabled" "false")" && echo "$service_name" || true
-		done | pipe_service_attribute host | tr '\n' ' ')
+		done | pipe_service_attribute host | join)
 else
 	on_vpn_cn=
 	on_vpn_gw=
@@ -269,34 +281,26 @@ fi
 
 
 if is_function_available "get_active_ugw_connections"; then
-	ugw_status_centralips=$(for gw in $(uci_get on-usergw.@usergw[0].centralIP); do ip route get $gw 2>/dev/null | awk '/dev $MESH_OPENVPN_DEVICE_PREFIX/ {printf $1" "}'; done)
-	on_ugw_status="$([ "$(echo "$ugw_status_centralips" | wc -w)" -ge "1" ] && echo "1" || echo "0")"
-	on_ugw_enabled="$(uci_is_true "$(uci_get on-usergw.ugw_sharing.shareInternet)" && echo "1" || echo "0")"
-
-	index=1
-	ugw_status_sharing_possible=0
-	while [ -n "$(uci_get on-usergw.opennet_ugw${index})" ]; do
-	  wan_status=$(uci_get on-usergw.opennet_ugw${index}.wan_status)
-	  mtu_status=$(uci_get on-usergw.opennet_ugw${index}.mtu_status)
-	  uci_is_true "$wan_status" && uci_is_true "$mtu_status" && ugw_status_sharing_possible=1 && break
-	  : $((index++))
-	done
-	on_ugw_possible="$ugw_status_sharing_possible"
-	on_ugw_tunnel="$([ -n "$(ls /tmp/opennet_ugw*.txt 2>/dev/null)" ] && echo "1" || echo "0")"
-	on_ugw_connected="${ugw_status_centralips# }"
-	on_ugw_presetips="$(uci_get on-usergw.@usergw[0].centralIP)"
-	on_ugw_presetnames="$(uci_get on-usergw.@usergw[0].name)"
+	# ist einer mesh-Dienste vollstaendig positiv getestet?
+	on_ugw_possible=$(get_services mesh | pipe_service_attribute "status" | while read status; do uci_is_true "$status" && echo "." || true; done | grep -q . && echo "1" || echo "0")
+	on_ugw_tunnel="$([ -n "$(get_active_ugw_connections)" ] && echo "1" || echo "0")"
+	# ermittle alle Nachbarn, die via tap-Interface verbunden sind - dies ist etwas ungenau, aber besser geht es wohl nicht
+	on_ugw_connected=$(echo /neighbors | nc localhost 2006 | grep "^[0-9]" | awk '{print $1}' | while read neighbor; do ip route get "$neighbor" 2>/dev/null | awk '/dev '$MESH_OPENVPN_DEVICE_PREFIX'/ {print $1}'; done | join)
+	_on_ugw_services=$(get_services mesh | filter_enabled_services | sort_services_by_priority)
+	on_ugw_presetnames=$(echo "$_on_ugw_services" | pipe_service_attribute "host")
+	# wir nehmen jeweils die erste IP der Namensaufloesung (typischer IPv4)
+	on_ugw_presetips=$(echo "$_on_ugw_services" | pipe_service_attribute "host" | while read host; do query_dns "$host" | head -1; done)
+	on_ugw_status="$([ -n "$on_ugw_connected" ] && echo "1" || echo "0")"
+	on_ugw_enabled="$(is_on_module_installed_and_enabled "on-usergw" && echo "1" || echo "0")"
 else
-	ugw_status_centralips=
-	on_ugw_status=
-	on_ugw_enabled=
-
 	ugw_status_sharing_possible=
 	on_ugw_possible=
 	on_ugw_tunnel=
 	on_ugw_connected=
-	on_ugw_presetips=
 	on_ugw_presetnames=
+	on_ugw_presetips=
+	on_ugw_status=
+	on_ugw_enabled=
 fi
 
 
@@ -312,20 +316,20 @@ if [ "$EXPORT_JSON" = "1" ];then
 "on_old_autoadapttxpwr":"%s","on_old_remoteconf":"%s","db_time":"%s","db_epoch":"%s","db_ver":"%s","db_update":"%s"}}\n
 EOF
 )
-    printf $JSON "" "$on_olsr_mainip" "$sys_ver" "${sys_board% }" "${sys_cpu% }" "$sys_mem" "$sys_uptime" "$sys_load" "$sys_free" "$sys_watchdog" "$sys_os_type" \
-	"${sys_os_name% }" "$sys_os_rel" "$sys_os_ver" "$sys_os_arc" "$sys_os_insttime" "$on_core_ver" "$on_core_insttime" "${on_packages% }" "$on_id" "$on_olsrd_status" \
+    printf $JSON "" "$on_olsr_mainip" "$sys_ver" "$sys_board" "$sys_cpu" "$sys_mem" "$sys_uptime" "$sys_load" "$sys_free" "$sys_watchdog" "$sys_os_type" \
+	"$sys_os_name" "$sys_os_rel" "$sys_os_ver" "$sys_os_arc" "$sys_os_insttime" "$on_core_ver" "$on_core_insttime" "$on_packages" "$on_id" "$on_olsrd_status" \
 	"$on_olsr_mainip" "$on_wifidog_status" "$on_wifidog_id" "$on_vpn_cn" "$on_vpn_status" "$on_vpn_gw" "$on_vpn_autosearch" "$on_vpn_sort" "$on_vpn_gws" \
 	"$on_vpn_blist" "$on_ugw_status" "$on_ugw_enabled" "$on_ugw_possible" "$on_ugw_tunnel" "$on_ugw_connected" "$on_ugw_presetips" "$on_ugw_presetnames" "" "" \
-	"$db_time" "$db_epoch" "$DATABASE_VERSION" "$db_epoch" | tr '\n' ' ' >>${DATABASE_FILE}.json.tmp
+	"$db_time" "$db_epoch" "$DATABASE_VERSION" "$db_epoch" | join >>${DATABASE_FILE}.json.tmp
     echo >>${DATABASE_FILE}.json.tmp
     mv -f ${DATABASE_FILE}.json.tmp ${DATABASE_FILE}.json
 else
   SQL_STRING="$SQL_STRING
     INSERT INTO nodes VALUES
-    ('', '$on_olsr_mainip', '$sys_ver', '${sys_board% }', '${sys_cpu% }',
+    ('', '$on_olsr_mainip', '$sys_ver', '$sys_board', '$sys_cpu',
     '$sys_mem', '$sys_uptime', '$sys_load', '$sys_free', '$sys_watchdog',
-    '$sys_os_type', '${sys_os_name% }', '$sys_os_rel', '$sys_os_ver', '$sys_os_arc',
-    '$sys_os_insttime', '$on_core_ver', '$on_core_insttime', '${on_packages% }',
+    '$sys_os_type', '$sys_os_name', '$sys_os_rel', '$sys_os_ver', '$sys_os_arc',
+    '$sys_os_insttime', '$on_core_ver', '$on_core_insttime', '$on_packages',
     '$on_id', '$on_olsrd_status', '$on_olsr_mainip',
     '$on_wifidog_status', '$on_wifidog_id', '$on_vpn_cn', '$on_vpn_status',
     '$on_vpn_gw', '$on_vpn_autosearch', '$on_vpn_sort', '$on_vpn_gws', '$on_vpn_blist',

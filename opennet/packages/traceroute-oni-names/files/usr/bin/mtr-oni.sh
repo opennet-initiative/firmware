5f5e3121ed2a221f6539fce8dd269e91637621ec
Martin Garbe <monomartin@on-i.de>
2015-10-06 21:35:05 +0200

    reverted changes to tracroute_oni package because it was broken. Need to fix the changes from Lars and then recommit it.

--- opennet/packages/traceroute-oni-names/files/usr/bin/mtr-oni.sh
+++ opennet/packages/traceroute-oni-names/files/usr/bin/mtr-oni.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-set -eu
-
-if [ "$#" -lt 2 ] || [ -z "$1" ]; then
+if [  -z $1 ]; then
    echo "Bad parameter! IP as parameter is needed."
    echo "e.g."
    echo "      mtr-oni.sh 192.168.0.33"
    echo
    exit
-fi >&2
+fi
 
-oni_tracert_mtr_helper.sh "$1" method-mtr
+source oni_tracert_mtr_helper.sh $1 method-mtr
--- opennet/packages/traceroute-oni-names/files/usr/bin/traceroute-oni.sh
+++ opennet/packages/traceroute-oni-names/files/usr/bin/traceroute-oni.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-set -eu
-
-if [ "$#" -lt 2 ] || [ -z "$1" ]; then
+if [  -z $1 ]; then
    echo "Bad parameter! IP as parameter is needed."
    echo "e.g."
    echo "      traceroute-oni.sh 192.168.0.33"
    echo
-   exit 1
-fi >&2
+   exit
+fi
 
-oni_tracert_mtr_helper.sh "$1" method-traceroute
+source oni_tracert_mtr_helper.sh $1 method-traceroute

c9c99987e29c9615d17b716ed94f82c6a0b83333
Martin Garbe <monomartin@on-i.de>
2015-10-11 23:47:54 +0200

    Recommit Lars changed with bugfixes

--- opennet/packages/traceroute-oni-names/files/usr/bin/oni_tracert_mtr_helper.sh
+++ opennet/packages/traceroute-oni-names/files/usr/bin/oni_tracert_mtr_helper.sh
@@ -2,137 +2,84 @@
 
 # $1 = IP address
 # $2 = ( method-traceroute | method-mtr )
+CONTACT_EMAIL="monomartin@on-i.de"
 
-rand=2466321 #"random" number for filenames. Hopefully no files created by users will be overwritten this way.
+dst_ip=$1
 
-#Important: Always check user input and replace by own string for security reasons
-progname="" #used later for calling programs
-if [ $2 = "method-traceroute" ]; then
+if [ "$2" = "method-traceroute" ]; then
     progname="traceroute"
-elif [ $2 = "method-mtr" ]; then
+elif [ "$2" = "method-mtr" ]; then
     progname="mtr"
+else
+    progname=""
 fi
-if [  -z $1 ] || [ -z $2 ] || { [ $2 != "method-traceroute" ] && [ $2 != "method-mtr" ]; }; then
+
+if [ "$#" -ne 2 ] || [ -z "$1" ] || [ -z "$progname" ]; then
    echo "Bad parameter! IP and type of program (traceroute|mtr) as parameter is needed."
    echo "e.g."
    echo "      ./oni-tracert-mtr-helper.sh 192.168.0.33 method-traceroute"
    echo "or"
    echo "      ./oni-tracert-mtr-helper.sh 192.168.0.33 method-mtr"
    echo
-   exit
-fi
+   exit 1
+fi >&2
 
-#check for OS because of 'expr' command. We need the GNU version, not BSD (MacOS) version
-os=$(uname)
-cmd_expr="expr"
-if [ "$os" == "Darwin" ]; then
-    #set patch for GNZ version of 'expr' on MAC
-    cmd_expr="/opt/local/libexec/gnubin/expr"
-fi
+gen_ip_list() {
+  local ip=$1
+  if [ "$progname" = "traceroute" ]; then
+      # replace any "*" (unknown) with space
+      traceroute -n "$ip" | tr '*' ' ' | while read line; do
+          # Ausgabe (Debugging)
+          #echo "$line"
+          
+          # print IP
+          echo "$line" | awk '{ print $2 }'
+      done
+  else
+      # ignore first lines ("HOST:"; "Start:")
+      mtr --no-dns --report --report-cycles=5 "$ip" | grep -v "^[A-Z]" | while read line; do
+          # Ausgabe (Debugging)
+          echo "$line"
+          echo "$line" | awk '{ print $2 }'
+      done
+  fi | grep -v "^$" | tr '\n' ','
+}
 
 echo "Please wait a second..."
 echo "---plain $progname output---"
 echo
-file_bin="/usr/sbin/$progname"
-file_sbin="/usr/sbin/$progname"
 
-type $progname >> /dev/null #check if traceroute or mtr are available
-ret=$?
-ip_list=""
-traceroute_file="/tmp/traceroute-$rand.log"
-if [ ret ]; then
-    if [ $progname = "traceroute" ]; then
-        echo "traceroute to $1" > $traceroute_file
-        traceroute $1 | tee -a $traceroute_file
-
-        while read line; do
-            #test for first line of traceroute. This line should be ignored
-            tmp=$($cmd_expr match "$line" 'traceroute to')
-            if [ "$tmp" != "0" ]; then
-                continue
-            fi
-
-            #extract IPs
-            t1=$(echo $line | cut -d " " -f 3)
-            len=$(( ${#t1} - 2 ))
-            ip=$($cmd_expr substr $t1 2 $len)
-            if [ ${#ip_list} = 0 ]; then
-                ip_list=$ip
-            else
-                ip_list="$ip_list,$ip"
-            fi
-        done < $traceroute_file
-
-    fi
-    if [ $progname = "mtr" ]; then
-        mtr -n --report --report-cycles=5 $1 | tee $traceroute_file
-
-        while read line; do
-            #test for first line of traceroute. This line should be ignored
-            tmp=$($cmd_expr match "$line" 'HOST:')
-            if [ "$tmp" != "0" ]; then
-                continue
-            fi
-            tmp=$($cmd_expr match "$line" 'Start:')
-            if [ "$tmp" != "0" ]; then
-                continue
-            fi
-
-
-            #extract IPs
-            ip=$(echo $line | cut -d ' ' -f 2)
-            if [ "$ip" == "???" ]; then
-                continue
-            fi
-            if [ ${#ip_list} = 0 ]; then
-                ip_list=$ip
-            else
-                ip_list="$ip_list,$ip"
-            fi
-        done < $traceroute_file
-    fi
-fi
+type "$progname" >/dev/null 2>&1 || {
+    echo "No traceroute program ($progname) found."
+    exit 2
+} >&2
 
 echo 
 echo "----------------------------"
 echo -n "Fetching Opennet Names. Please wait ..."
 
+ip_list=$(gen_ip_list $dst_ip)
+ip_list=$(echo $ip_list | sed "s/^to,//") #delete first line with "traceroute to ..."
+ip_list=${ip_list%,} #delete last "," which is a result of loop
 
 #---TODO---URL von www.on-i.de holen oder von api.on-i.de ----
 #          die URL dann in config speichern, falls das einmalige abholen lange dauert
 
 server=yurika.on-i.de #TODO change this
-ret_file="/tmp/ret-$rand.txt"
-wget -q -O $ret_file http://$server/api/tracerouteoninames/traceroute-oni.php?input=$ip_list &>/dev/null
+response=$(wget -q -O - "http://$server/api/tracerouteoninames/traceroute-oni.php?input=$ip_list" 2>/dev/null)
 
-if [ ! -f  $ret_file ]; then
-        echo
-        echo
-        echo "Error! Cannot connect to web service. Sorry. Try again later or contact monomartin@on-i.de"
-        echo
-        exit
-else
-    out=$(head -1 $ret_file)
-    if [ ! -n "$out" ]; then
-        echo
-        echo
-        echo "Error! Cannot connect to web service. Sorry. Try again later or contact monomartin@on-i.de"
-        echo
-        exit
-    fi
-fi
+# file does not exist or is empty?
+if [ -z "$response" ]; then
+    echo
+    echo
+    echo "Error! Cannot connect to web service. Sorry. Try again later or contact $CONTACT_EMAIL"
+    echo
+    exit 3
+fi >&2
 
 echo "  finished."
 
-#echo "Now cat-ting /tmp/ret.txt..."
+# output of the web API
 echo
-cat $ret_file
+echo "$response"
 echo
-
-#delete tmp files
-if [ -f $traceroute_file ]; then
-    rm $traceroute_file
-fi
-if [ -f $ret_file ]; then
-    rm $ret_file
-fi
--- opennet/packages/traceroute-oni-names/files/usr/bin/traceroute-oni.sh
+++ opennet/packages/traceroute-oni-names/files/usr/bin/traceroute-oni.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if [  -z $1 ]; then
+set -eu
+
+if [ "$#" -lt 1 ] || [ -z "$1" ]; then
    echo "Bad parameter! IP as parameter is needed."
    echo "e.g."
    echo "      traceroute-oni.sh 192.168.0.33"
    echo
-   exit
-fi
+   exit 1
+fi >&2
 
-source oni_tracert_mtr_helper.sh $1 method-traceroute
+oni_tracert_mtr_helper.sh "$1" method-traceroute

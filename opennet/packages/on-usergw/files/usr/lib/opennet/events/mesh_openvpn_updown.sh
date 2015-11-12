78920e4b3fa7bfd549fec614ff1e08f20ffd3a87
Lars Kruse <devel@sumpfralle.de>
2015-11-12 02:41:57 +0100

    Mesh-VPN: Neukonfiguration von olsrd zeitlich vom VPN-Verbindungsaufbau entkoppeln
    Die Netzwerk-Konfiguration dauert manchmal länger - dies verhinderte
    gelegentlich den olsr-Betrieb auf dem VPN-Interface.

--- opennet/packages/on-usergw/files/usr/lib/opennet/events/mesh_openvpn_updown.sh
+++ opennet/packages/on-usergw/files/usr/lib/opennet/events/mesh_openvpn_updown.sh
@@ -27,31 +27,26 @@ setup_mesh_interface() {
 	netname=$(get_netname "$ifname")
 	uci set "network.${netname}=interface"
 	uci set "network.${netname}.proto=none"
-	uci set "network.${netname}.auto=0"
 	# wir duerfen das Interface nicht via uci hinzufuegen - andernfalls verliert das Interface durch netifd seine Konfiguration
 	# siehe https://lists.openwrt.org/pipermail/openwrt-devel/2015-June/033501.html
 	#uci set "network.${netname}.ifname=$ifname"
+	ubus call network reload
 	add_interface_to_zone "$ZONE_MESH" "$netname"
 	apply_changes network firewall
-	# Falls wir hier nicht warten, wird olsrd zu frueh neugestartet (bevor tapX aktiv ist).
-	# Oft reicht eine Sekunde - aber wir wollen lieber sichergehen und wissen nicht, worauf
-	# wir achten sollten.
-	sleep 2
 	# indirekte Interface/Network-Zuordnung (siehe obigen Mailinglisten-Beitrag)
 	# Auf diesem Weg bleibt die IP-Konfiguration des Device erhalten.
 	local ubus_dev="network.interface.${netname}"
 	ubus call "$ubus_dev" add_device '{ "name": "'$ifname'" }'
 	# die obige ubus-Aktion wird nebenlaeufig abgearbeitet - wir muessen das Ergebnis abwarten
-	ubus wait_for "$ubus_dev"
-	# Auch diese Pause ist nicht ganz klar - aber sie scheint dafuer zu sorgen, dass olsrd auf den Interfaces laeuft.
-	sleep 2
-	update_olsr_interfaces
+	ubus -t 10 wait_for "$ubus_dev"
 	# expliziter olsrd-Neustart: eventuell sind noch Fragmente alter tap-Devices in
 	# der olsrd-Konfiguration eingetragen. Diese verhindern einen olsrd-Neustart,
 	# da es scheinbar keine Änderung gab.
 	/etc/init.d/olsrd restart
 	# ohne dieses explizite reload reagiert die firewall seltsamerweise nicht auf die neuen Interfaces
 	/etc/init.d/firewall reload
+	# iu Kuerze moege die olsr-Interface-Liste neu erstellt werden (inkl. des neuen Interface)
+	echo "on-function update_olsr_interfaces" | schedule_task
 }
 
 

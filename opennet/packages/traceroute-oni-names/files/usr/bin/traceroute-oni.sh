9ddf1e2df399e2e044cea283940338469699517c
Martin Garbe <monomartin@opennet-initiative.de>
2016-01-10 22:04:20 +0100

    rewrite of oni-traceroute-cli tool. Now using API. Reducing tool to ONE shell skript.

--- opennet/packages/traceroute-oni-names/files/usr/bin/traceroute-oni.sh
+++ opennet/packages/traceroute-oni-names/files/usr/bin/traceroute-oni.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+dst_ip=$1
+
 if [ "$#" -lt 1 ] || [ -z "$1" ]; then
    echo "Bad parameter! IP as parameter is needed."
    echo "e.g."
@@ -10,4 +12,36 @@ if [ "$#" -lt 1 ] || [ -z "$1" ]; then
    exit 1
 fi >&2
 
-oni_tracert_mtr_helper.sh "$1" method-traceroute
+get_location_from_api() {
+  local ip=$1
+  #extract value of post_address
+  # sample input:   "post_address":"xyz"
+  # sample output:  xyz
+  location=$(wget -q -O - http://api.on/api/v1/interface/$ip/accesspoint/ | awk 'BEGIN { FS = "post_address\":\"" } ; { print $2 }' | cut -d"\"" -f1)
+  echo -n "$location"
+}
+
+traceroute "$dst_ip" | tr '*' ' ' | while read line; do
+
+  #ignore first line "traceroute to ...."
+  tmp=$(echo $line | awk '{print substr($0,0,13)}')
+  if [ "$tmp" = "traceroute to" ]; then
+    continue
+  fi
+
+  num=$(echo "$line" | awk '{ print $1 }')
+  dns=$(echo "$line" | awk '{ print $2 }')
+  ip=$(echo "$line" | awk '{ print $3 }')
+  ip=${ip%)} #delete last ")"
+  ip=${ip#(} #delete first "("
+
+  first_num=$(echo $ip | cut -d"." -f1 )
+  if [ "$first_num" = "192" -o "$first_num" = "10" ]; then # "-o" means "or" here
+    #only process 10.x.y.z and 192.168.x.y.z IPs. Ignore 172.x.y.z and lines with "*" symbol
+    # fetch location name
+    echo -n " $num - IP: $ip - Location: "
+    get_location_from_api $ip
+    echo " - DNS: $dns"
+  fi
+done
+

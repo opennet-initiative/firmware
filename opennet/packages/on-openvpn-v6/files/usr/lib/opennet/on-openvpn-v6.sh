0e47d59b6f07fd7eb746711e7d0aff6d7e033972
Martin Garbe <monomartin@opennet-initiative.de>
2020-04-07 19:29:57 +0200

    ermoegliche auch ueber IPv4 eine Verbindung zum OpenVPN Server aufzubauen

--- opennet/packages/on-openvpn-v6/files/usr/lib/opennet/on-openvpn-v6.sh
+++ opennet/packages/on-openvpn-v6/files/usr/lib/opennet/on-openvpn-v6.sh
@@ -4,7 +4,9 @@ NETWORK_TUNNEL_V6=on_vpn_v6
 #OPENVPN_CONFIG_BASEDIR=/var/etc/openvpn #Variable sollte aus on-openvpn bekannt sein
 SERVICE_NAME=gw_openvpn_v6_fd32_d8d3_87da__245_1700_udp
 SERVICE_TYPE=gw
-REMOTE_ADDR=fd32:d8d3:87da::245
+REMOTE_ADDR_V6=fd32:d8d3:87da::245
+REMOTE_ADDR_V4=192.168.0.245
+REMOTE_PORT=1700
 
 MIG_OPENVPN_V6_CONFIG_TEMPLATE_FILE=/usr/share/opennet/openvpn-v6-mig.template
 PID_FILE=/var/run/${SERVICE_NAME}.pid
@@ -105,7 +107,7 @@ delete_openvpn_v6_service() {
 	uci delete "${uci_prefix}"
 	apply_changes openvpn
 
-	rm "$cnfig_file"
+	rm "$config_file"
 	# loesche Verzeichnis wenn es leer ist
 	rmdir --ignore-fail-on-non-empty "$OPENVPN_V6_CONFIG_BASEDIR"
 }
@@ -118,10 +120,20 @@ disable_openvpn_v6_service() {
 
 
 ## liefere openvpn-Konfiguration eines Dienstes zurück
+## param: 'v6' oder 'v4' - abhängig davon wird der OpenVPN Server mit IPv6 oder IPv4 angesprochen
+##        wird kein Wert angegeben, wird automatisch die IPv6 Adresse genommen
 get_openvpn_v6_config() {
 	trap 'error_trap get_openvpn_v6_config "$*"' EXIT
+	local ip_version="$1"
 	local service_name=$SERVICE_NAME
 	local template_file=$MIG_OPENVPN_V6_CONFIG_TEMPLATE_FILE
+	if [ -n "$ip_version" ] && [ "$ip_version" = "v4" ]; then
+		echo "remote $REMOTE_ADDR_V4 $REMOTE_PORT"
+		echo "proto udp"
+	else
+		echo "remote $REMOTE_ADDR_V6 $REMOTE_PORT"
+		echo "proto udp6"
+	fi
 	cat "$template_file"
 	# sicherstellen, dass die Konfigurationsdatei mit einem Zeilenumbruch endet (fuer "echo >> ...")
 	echo
@@ -136,7 +148,16 @@ update_vpn_v6_config() {
 	service_add_file_dependency "$service_name" "$config_file"
 	# Konfigurationsdatei neu schreiben
 	mkdir -p "$(dirname "$config_file")"
-	get_openvpn_v6_config "$service_name" >"$config_file"
+	get_openvpn_v6_config "v6" >"$config_file"
 }
 
 
+## wenn keine IPv6 Verbindung zum OpenVPN Server moeglich ist, muss sich per IPv4 verbunden werden
+connect_vpn_v6_server_via_v4() {
+	trap 'error_trap connect_vpn_v6_server_via_v4 "$*"' EXIT
+	local service_name="$SERVICE_NAME"
+	#TODO hier haben wir jetzt einen Dateinamen mit IPv6 Adressen im Namen aber IPv4 Adresse in der Datei. Verbessern.
+	local config_file="$OPENVPN_V6_CONFIG_BASEDIR/${service_name}.conf"
+	#ersetze remote addr in config Datei
+	get_openvpn_v6_config "v4" >"$config_file"
+}

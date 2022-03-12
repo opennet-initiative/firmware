ca88ae19cfa9eda366bbb71164739fe6fe740f07
Martin Garbe <monomartin@opennet-initiative.de>
2022-03-12 11:44:14 +0100

    improve directory handling on export

--- opennet/tools/buildbot-worker/export-build-buildbot.sh
+++ opennet/tools/buildbot-worker/export-build-buildbot.sh
@@ -3,18 +3,15 @@
 # stop on error and unset variables
 set -eu
 
-BUILD_DIR="../../.."
+BUILD_DIR="."
 BIN_DIR="$BUILD_DIR/openwrt/bin"
 DOC_DIR="$BUILD_DIR/build-doc/api/html"
+OPENWRT_CONFIG="$BUILD_DIR/openwrt/.config"
 
 EXPORT_SERVER="ruri.on"
 EXPORT_SSH_USER=trac-bitten-slave
 EXPORT_DIR="~/export"  # this folder is on remote download server
 RSYNC_OPTIONS="--archive"
-
-# get current script dir
-HOME="$(dirname $(readlink -f "$0"))"
-OPENWRT_CONFIG="${HOME}/../../../openwrt/.config"
 
 snapshot_name=$(grep "^CONFIG_VERSION_NUMBER=" "$OPENWRT_CONFIG" | cut -f 2 -d '"')
 [ -z "$snapshot_name" ] && return 1
@@ -28,7 +25,7 @@ echo $commit_info > "$BIN_DIR/__${snapshot_name}__"
 echo "$snapshot_name" >"$BIN_DIR/version.txt"
 
 # generate a map between devices and upgrade firmware download paths
-"$HOME/$BUILD_DIR/opennet/tools/device-upgrade-mapper.sh" generate \
+"$BUILD_DIR/opennet/tools/device-upgrade-mapper.sh" generate \
 	>"$BIN_DIR/device-upgrade-map.csv"
 
 rsync -az -e "ssh -i /var/lib/buildbot/.ssh/ssh_host_rsa_key -o PasswordAuthentication=no" $BIN_DIR $EXPORT_SSH_USER@$EXPORT_SERVER:$EXPORT_DIR/$snapshot_name/

9994802fbf9322fafc50e6e29bad0b61e263edc3
Mathias <3380095+mathiashro@users.noreply.github.com>
2022-04-18 08:22:13 +0200

    tools: export opennet documentation during buildbot export job
    Add the documentation output before the rsync process. This copies the old trac-bitten behavior now also to the buildbot export process.

--- opennet/tools/buildbot-worker/export-build-buildbot.sh
+++ opennet/tools/buildbot-worker/export-build-buildbot.sh
@@ -23,17 +23,26 @@ snapshot_name=$(grep "^CONFIG_VERSION_NUMBER=" "$OPENWRT_CONFIG" | cut -f 2 -d '
 [ -z "$snapshot_name" ] && return 1
 
 # set version number in export directories
+echo "Generate snapshot version file ..."
 commit_info=$(git log --oneline | sed -n '1!G;h;$p' | nl | tac)
 echo $commit_info > "$BIN_DIR/__${snapshot_name}__"
 
 # allow retrieval of "latest" version by clients
 # URL: https://downloads.opennet-initiative.de/openwrt/stable/latest/version.txt
+echo "Generate version TXT file ..."
 echo "$snapshot_name" >"$BIN_DIR/version.txt"
 
 # generate a map between devices and upgrade firmware download paths
+echo "Generate device upgrade CVS file ..."
 "$BUILD_DIR/opennet/tools/device-upgrade-mapper.sh" generate \
 	>"$BIN_DIR/device-upgrade-map.csv"
 
+# include documentation into the firmware export
+echo "Preparing documentation files for export directory ..."
+mkdir -p "$BIN_DIR/doc"
+rsync $RSYNC_OPTIONS "$DOC_DIR/" "$BIN_DIR/doc"
+
+# transfer all data to external server
 echo "Copy generated files to download server ..."
 rsync $RSYNC_OPTIONS -e "ssh -i /var/lib/buildbot/.ssh/$EXPORT_SSH_KEY -o PasswordAuthentication=no" $BIN_DIR/ $EXPORT_SSH_USER@$EXPORT_SERVER:$EXPORT_DIR/$snapshot_name/
 
